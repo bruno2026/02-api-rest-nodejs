a3f038f7decbc4a58ab495039d8b445a713f1200
Bruno Gomes Ferreira <brunogomes@sisconultoria.com.br>
2023-04-19 19:01:21 -0300

    Create of transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,25 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const tables = knex('sqlite_schema').select('*')
-    return tables
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
